d55ef166ea7d01065471c5152c0a14f937385b6a
EC2 Default User <ec2-user@ip-172-31-46-160.ap-northeast-1.compute.internal>
2023-06-04 13:48:28 +0000

    [finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,6 +27,12 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists' # indexのところ。　投稿一覧へ。
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
